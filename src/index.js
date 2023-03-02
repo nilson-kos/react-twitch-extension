bf495b666b4875ac4b3066e8cd680fe4ef63e2b0
kostiantynshcherbyna <k.shcherbyna@weplay.tv>
2023-03-03 00:53:33 +0200

    add react to twitch extension

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
 function onAuth(uid) {
   const element = document.createElement('div');
   element.innerHTML = 'Twitch onAuthorized called with UID: ' + uid;
@@ -13,3 +16,11 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+function Hello(props) {
+  return React.createElement('div', null, 'Hello ', props.toWhat);
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    React.createElement(Hello, { toWhat: 'World' }, null)
+);
